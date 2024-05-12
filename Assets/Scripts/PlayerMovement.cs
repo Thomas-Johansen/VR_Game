a3800937034a949c76fd3a60d9504a1011f3c48f
Thomas-Johansen <thomassandjoh@hotmail.com>
2024-05-12 10:18:27 +0200

    Added basic jump in walk mode. (It's a game where you can fly, being able to jump while in the air isn't to big a problem)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector3 _currentSpeed;
-    private Vector3 _deltaSpeed;
-
+    private bool _isFlying = false;
+    private bool _doJump = false;
     private int _walkSpeed = 500;
+    private int _jumpStrength = 250;
 
 
     public Rigidbody playerBody;
@@ -17,19 +21,44 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Camera playerCamera;
 
     [SerializeField] private InputActionAsset inputActions;
-    private InputAction _moveAction;
-    private Vector2 _moveVector;
+    private InputAction _leftMoveAction;
+    private Vector2 _leftMoveVector;
+    private InputAction _leftPrimaryAction;
+    private bool _leftPrimary;
+    private InputAction _leftSecondaryAction;
+    private bool _leftSecondary;
+    private InputAction _rightMoveAction;
+    private Vector2 _rightMoveVector;
 
 
     private void Awake()
     {
-        InputActionMap actionMap = inputActions.FindActionMap("XRI LeftHand Locomotion");
-        _moveAction = actionMap.FindAction("Move");
-        _moveAction.Enable();
-        _moveAction.performed += OnMovementPerformed;
-        _moveAction.canceled += OnMovementCanceled;
-    }
+        // Bindings
+        // Headset
+        
+        // Right Hand
+        InputActionMap rightHandLocomotion = inputActions.FindActionMap("XRI RightHand Locomotion");
+        _rightMoveAction = rightHandLocomotion.FindAction("Move");
+        _rightMoveAction.Enable();
+        _rightMoveAction.performed += OnRightMovementPerformed;
+        _rightMoveAction.canceled += OnRightMovementCanceled;
+        
+        // Left Hand
+        InputActionMap leftHandLocomotion = inputActions.FindActionMap("XRI LeftHand Locomotion");
+        _leftMoveAction = leftHandLocomotion.FindAction("Move");
+        _leftMoveAction.Enable();
+        _leftMoveAction.performed += OnLeftMovementPerformed;
+        _leftMoveAction.canceled += OnLeftMovementCanceled;
+        _leftPrimaryAction = leftHandLocomotion.FindAction("X Button");
+        _leftPrimaryAction.Enable();
+        _leftPrimaryAction.performed += OnLeftPrimaryPerformed;
+        _leftPrimaryAction.canceled += OnLeftPrimaryCanceled;
+        _leftSecondaryAction = leftHandLocomotion.FindAction("Y Button");
+        _leftSecondaryAction.Enable();
+        _leftSecondaryAction.performed += OnLeftSecondaryPerformed;
+        _leftSecondaryAction.canceled += OnLeftSecondaryCanceled;
 
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -46,25 +75,82 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 playerUp = transform.up;
-        Vector3 direction = playerCamera.transform.forward;
-        Vector3 right = Vector3.Cross(playerUp, direction).normalized;
-        Vector3 correctedDirection = Vector3.Cross(right, playerUp).normalized;
+
+        if (!_isFlying) // Walking Code
+        {
+            if (_leftMoveVector != Vector2.zero)
+            {
+                Vector3 playerUp = transform.up;
+                Vector3 direction = playerCamera.transform.forward;
+                Vector3 right = Vector3.Cross(playerUp, direction).normalized;
+                Vector3 correctedDirection = Vector3.Cross(right, playerUp).normalized;
         
-        float forwards = _moveVector.y;
-        float sideways = _moveVector.x;
-        Vector3 movement = (correctedDirection * forwards + right * sideways).normalized;
-        playerBody.AddForce((movement * (Time.fixedDeltaTime * _walkSpeed)));
+                float forwards = _leftMoveVector.y;
+                float sideways = _leftMoveVector.x;
+                Vector3 movement = (correctedDirection * forwards + right * sideways).normalized;
+                playerBody.AddForce((movement * (Time.fixedDeltaTime * _walkSpeed))); 
+            }
+
+            if (_doJump)
+            {
+                _doJump = false;
+                Debug.Log("Jumping");
+                playerBody.AddForce(transform.up * _jumpStrength);
+            }
+            
+        }
+        else // Flying Code
+        {
+            
+        }
+        
+        //Universal Controls
+        
     }
 
-    private void OnMovementPerformed(InputAction.CallbackContext context)
+    //Right Hand
+    private void OnRightMovementPerformed(InputAction.CallbackContext context)
     {
-        _moveVector = context.ReadValue<Vector2>();
+        _rightMoveVector = context.ReadValue<Vector2>();
     }
     
-    private void OnMovementCanceled(InputAction.CallbackContext context)
+    private void OnRightMovementCanceled(InputAction.CallbackContext context)
     {
-        _moveVector = Vector2.zero;
+        _rightMoveVector = Vector2.zero;
+    }
+    
+    //LeftHand
+    private void OnLeftMovementPerformed(InputAction.CallbackContext context)
+    {
+        _leftMoveVector = context.ReadValue<Vector2>();
+    }
+    
+    private void OnLeftMovementCanceled(InputAction.CallbackContext context)
+    {
+        _leftMoveVector = Vector2.zero;
+    }
+    
+    private void OnLeftPrimaryPerformed(InputAction.CallbackContext context)
+    {
+        _leftPrimary = true;
+        _doJump = true;
+    }
+    
+    private void OnLeftPrimaryCanceled(InputAction.CallbackContext context)
+    {
+        _leftPrimary = false;
+        _doJump = false; //Edge case
+    }
+    
+    private void OnLeftSecondaryPerformed(InputAction.CallbackContext context)
+    {
+        _leftSecondary = true;
+        
+    }
+    
+    private void OnLeftSecondaryCanceled(InputAction.CallbackContext context)
+    {
+        _leftSecondary = false;
     }
     
 }
